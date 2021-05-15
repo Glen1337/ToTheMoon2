a205880637cc7b4d4bb1fb9337ec3cbb5bb31bb2
Glen Anderson <glen4096@gmail.com>
2021-05-14 22:51:26 -0400

    small changes, add loading spinner to app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
   title = 'ToTheMoon'
+  public currentlyLoading: boolean = true;
+
+  constructor(){
+    this.currentlyLoading = true;
+  }
+
+  ngAfterViewChecked(){
+    this.currentlyLoading = false;
+  }
+
 }
