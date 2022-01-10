f2193e3ef7bdf643027526f7c676b0cbcb576b64
Glen Anderson <glen4096@gmail.com>
2022-01-10 02:17:45 -0500

    realtime ticker work, added signalr package

--- src/app/ticker/ticker.component.ts
+++ src/app/ticker/ticker.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { concatMap, map, retry, Subscription } from 'rxjs';
 import { FinancialPage } from '../Common/FinancialPage';
 import { WatchItem } from '../Models/WatchItem';
 import { TickerService } from '../Services/ticker-service.service';
 import { WatchlistService } from '../Services/watchlist.service';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'ticker',
@@ -14,27 +16,54 @@ export class TickerComponent extends FinancialPage implements OnInit, OnDestroy 
 
   private url = 'https://cloud-sse.iexapis.com/stable/stocksUS5Second?token=sk_228dd2709a5341768a6920468437f264&symbols=snap';
   public watchList: WatchItem[] = [];
+  private baseUrl = environment.baseApiUrl;
 
-  constructor(private watchlistService: WatchlistService, private tickerService: TickerService) { 
+  constructor(private watchlistService: WatchlistService, private tickerService: TickerService, private http: HttpClient) { 
     super();
   }
 
   ngOnInit(): void {
-    let sub1 = this.watchlistService.getWatchList().
-    subscribe({
-      next: (response) => {
-        this.watchList = response;
-        this.tickerService.getServerSentEvent(this.url).
-        subscribe(
-          data => console.log(data)
-        );
-      },
-      error: (error) => {
-        console.log('(component)Error getting watchlist for ticker', error);
-      },
-      complete: () => { console.log('(component) watchlist retrieved for ticker');}
-    })
-    this.subscriptions.push(sub1)
+
+    // Add safe, URL encoded search parameter if there is a search term
+    let stocks: string[] = []
+
+
+    this.tickerService.startConnection();
+    this.tickerService.addQuoteListener();
+
+
+    this.http.get(`${this.baseUrl}watchitems/realtime`).subscribe(res => {
+      console.log(res);
+    });
+
+    // this.watchlistService.getWatchList().pipe(
+    //   //pluck(x => x.symbol),
+    //   map(x => x.map(watchItem => watchItem.symbol)),
+    //   retry(2)
+    // ).subscribe(watchItems => {
+    //   let stocks = encodeURIComponent(JSON.stringify(watchItems));
+    //   let options = { params: new HttpParams().set('stocks', stocks) };
+
+    // });
+    
+
+  
+
+    // let sub1 = this.watchlistService.getWatchList().
+    // subscribe({
+    //   next: (response) => {
+    //     this.watchList = response;
+    //     this.tickerService.getServerSentEvent(this.url).
+    //     subscribe(
+    //       data => console.log(data)
+    //     );
+    //   },
+    //   error: (error) => {
+    //     console.log('(component)Error getting watchlist for ticker', error);
+    //   },
+    //   complete: () => { console.log('(component) watchlist retrieved for ticker');}
+    // })
+    // this.subscriptions.push(sub1)
   }
 
 }
