7ce59c183c92b8c4c84d5b7ff45ed37db5283ac6
Glen Anderson <glen4096@gmail.com>
2022-01-16 15:30:37 -0500

    add tour, change navbar color varaibles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+declare function introJs(): any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'ToTheMoon'
   public currentlyLoading: boolean = true;
 
@@ -13,9 +14,9 @@ export class AppComponent{
     //this.currentlyLoading = true;
     
   }
-
-  // ngAfterViewChecked(){
-  //   this.currentlyLoading = false;
-  // }
+  ngOnInit(): void {
+    // Start tour
+    introJs().start();
+  }
 
 }
