4e11a5cced90442fc5408e46a5371c2a1c062759
Glen Anderson <glen4096@gmail.com>
2022-10-31 00:47:20 -0400

    footer changes, add spinner to app component for loading spinner between component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 declare function introJs(): any;
 @Component({
@@ -7,17 +8,36 @@ declare function introJs(): any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'ToTheMoon'
-  public currentlyLoading: boolean = false;
 
-  constructor() {}
+  public title = 'ToTheMoon'
+  // public currentlyLoading: boolean = false;
+  public isLoaded: boolean = false;
+  public isHttpLoaded: boolean = false;
+
+
+  constructor(private route: Router) {}
 
   // ngAfterViewInit(){
   //   this.currentlyLoading = false;
   // }
   ngOnInit(): void {
+
     // Start tour
     //this.currentlyLoading = true;
+    this.route.events.subscribe({
+      next: (event) => {
+      if (event instanceof NavigationStart) {
+        this.isLoaded = true;
+      } else if (event instanceof NavigationEnd) {
+        this.isLoaded = false;
+      }
+    },
+    error: (error) => {
+      this.isLoaded = false;
+      console.log(`Error in app component: ${error}`);
+    }
+  });
+
 
     introJs().start();
   }
