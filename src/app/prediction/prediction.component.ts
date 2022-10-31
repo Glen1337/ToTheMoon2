888562757d8ebbb227e7c54bd73c2d228f3ed798
Glen Anderson <glen4096@gmail.com>
2022-10-30 22:29:35 -0400

    cleanup

--- src/app/prediction/prediction.component.ts
+++ src/app/prediction/prediction.component.ts
@@ -73,44 +73,4 @@ export class PredictionComponent extends FinancialPage {
     return Object.keys(this.predictionResult).length === 0;
   }
 
-  // public submitForm(): void {
-  //   this.currentlyLoading = true;
-
-  //   let sub: Subscription = new Subscription();
-  //   let symbol: string = this.predictionControl?.value.trim().toUpperCase();
-
-  //   sub = this.predictionService.getPredictionResults(symbol).subscribe({
-  //     next: (data) => {
-  //       this.predictionResult = data as MLPrediction;
-  //       console.log(this.predictionResult);
-  //     },
-  //     error: (error) => {
-  //       this.errorMsg = error.error;
-  //       console.log('(component)Error getting ML prediction: ', error);
-  //     },
-  //     complete: () => {
-  //       '(component)ML prediction result received';
-  //       this.currentlyLoading = false;
-
-  //       setTimeout(() => {
-  //         // Bootstrap tooltip initialization
-  //         const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-  //         const tooltipList = tooltipTriggerList.map(tooltipTriggerEl => {
-  //           return new bootstrap.Tooltip(tooltipTriggerEl);
-  //         });
-
-  //         this.tooltipList.push(...tooltipList);
-
-  //         var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-  //         var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  //           return new bootstrap.Popover(popoverTriggerEl)
-  //         });
-
-  //         this.popoverList.push(...popoverList);
-  //       }, 400)
-  //     }
-  //   });
-
-  //   this.subscriptions.push(sub);
-  // }
 }
